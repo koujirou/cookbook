f217cb0df40f12e6d74c9b35b1f94016848dcb83
U-JP\kubokouj <kubokouj@d-kubokouj.jp.co.square-enix.com>
2013-04-05 12:53:02 +0900

    buildtool.rb

---
+++ misc/definitions/buildtool.rb
@@ -0,0 +1,26 @@
+define :buildtool_make_install do
+
+  params[:dstdir] = params[:srcdir] if params[:dstdir].nil? 
+  
+  case params[:file]
+  when /(^.*)(\.tar\.gz|\.tgz)/
+    execute "tarball" do
+    command "tar zxvf #{params[:srcdir]}#{params[:file]}  -C #{params[:dstdir]}"
+    end
+  end
+
+  configure_option = params[:configure].join(' ')
+  result = params[:file].match(/(^.*)(\.tar\.gz|\.tgz)/)
+
+  if result[1] != nil then 
+    execute "configure" do
+    command "cd #{params[:dstdir]}#{result[1]} && ./configure #{configure_option} && make"
+    end
+  end
+  if result[1] != nil then 
+    execute "make install" do
+    command "cd #{params[:dstdir]}#{result[1]} && make install"
+    end
+  end
+end
+
